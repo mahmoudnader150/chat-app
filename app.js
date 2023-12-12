b3e1d6a1b026a6bc896d5f521bb6b2440b34fc58
Mahmoud Nader Ali <91666764+mahmoudnader150@users.noreply.github.com>
2023-12-12 21:55:27 +0200

    Implementing on and emit functions ...basically

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = require('socket.io')(server, { cors: { origin: "*" } });
 
-// Handle incoming connections
-io.on('connection', (socket) => {
-    console.log('A user connected');
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views"); // Specify the directory where your views are stored
 
-    // Handle messages
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg); // Broadcast message to all connected clients
-    });
-
-    // Handle disconnections
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+app.get('/', function (req, res) {
+    res.render('home'); // Renders the 'home.ejs' file in the views directory
 });
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
+});
+
+io.on('connection', (socket) => {
+    console.log("user connected", socket.id);
+
+    socket.on('message', (data) => {
+        console.log(data)
+    })
 });
